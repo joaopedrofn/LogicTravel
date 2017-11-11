dada6d855c476814b3f6d913d8d82da12d2a57d3
joaopedrofn <joaopedrofn@gmail.com>
2017-11-11 16:08:08 -0300

    upload

---
+++ projeto.pl
@@ -0,0 +1,291 @@
+
+
+coordenadas(berlim, 52.507629, 13.1459643).
+coordenadas(bombaim, 19.0830944,72.7411171).
+coordenadas(brasilia, -15.786360, -47.893818).
+coordenadas(budapeste, -15.786360, -47.893818).
+coordenadas(buenosaires, -34.603750, -58.381332).
+coordenadas(cairo, 30.046706, 31.233058).
+coordenadas(compenhague, 55.675119, 12.574570).
+coordenadas(dubai, 25.213330, 55.277500).
+coordenadas(dublin, 53.350478, -6.264938).
+coordenadas(havana, 23.117557, -82.362249).
+coordenadas(hongkong, 22.283935, 114.170441).
+coordenadas(istambul, 41.015345, 28.976437).
+coordenadas(joanesburgo, -26.206009, 28.044142).
+coordenadas(lima, -12.046750, -77.048215).
+coordenadas(lisboa, 38.722180, -9.136174).
+coordenadas(londres, 51.506340, -0.129092).
+coordenadas(longyearbyen, 78.223645, 15.635516).
+coordenadas(losangeles, 34.049770, -118.285430).
+coordenadas(madri, 40.415916, -3.713878).
+coordenadas(marraquexe, 31.630510, -7.984510).
+coordenadas(melbourne, -37.815178, 144.965109).
+coordenadas(mexico, 23.767762, -103.131333).
+coordenadas(miami, 25.763139, -80.194614).
+coordenadas(montreal, 45.503121, -73.575615).
+coordenadas(moscou, 55.758747, 37.624228).
+coordenadas(novaiorque, 40.719884, -74.019239).
+coordenadas(oslo, 59.912719, 10.750091).
+coordenadas(paris, 48.855675, 2.347555).
+coordenadas(pequim, 39.913271, 116.397422).
+coordenadas(petrolina, -9.383850, -40.509367).
+coordenadas(recife, -8.056641, -34.891198).
+coordenadas(reykjavík, 64.126721, -21.816738).
+coordenadas(riodejaneiro, -22.906112, -43.181926).
+coordenadas(roma, 41.905898, 12.486550).
+coordenadas(salvador, -12.972886, -38.495341).
+coordenadas(saopaulo, -23.548782, -46.644779).
+coordenadas(singapura, 1.353169, 103.858680).
+coordenadas(toquio, 35.722642, 139.730384).
+coordenadas(ushuaia, -54.801427, -68.305208).
+
+route(berlim,budapeste,117).
+route(berlim,copenhague,232).
+route(berlim,dublin,365).
+route(berlim,istambul,213).
+
+route(bombaim,cairo,960).
+route(bombaim,dubai,364).
+route(bombaim,hongkong,1261).
+route(bombaim,istambul,1630).
+
+route(brasilia,buenosaires,2172).
+route(brasilia,lisboa,4453).
+route(brasilia,miami,7417).
+route(brasilia,recife,583).
+
+route(budapeste,berlim,136).
+route(budapeste,cairo,781).
+route(budapeste,copenhague,252).
+route(budapeste,dubai,1758).
+
+route(buenosaires,brasilia,2074).
+route(buenosaires,lima,1403).
+route(buenosaires,londres,8246).
+route(buenosaires,madri,4394).
+
+route(cairo,berlim,745).
+route(cairo,budapeste,708).
+route(cairo,copenhague,797).
+route(cairo,dubai,886).
+
+route(copenhague,berlim,138).
+route(copenhague,budapeste,134).
+route(copenhague,cairo,1097).
+route(copenhague,dubai,1727).
+
+route(dubai,bombaim,346).
+route(dubai,budapeste,2145).
+route(dubai,cairo,644).
+route(dubai,copenhague,2000).
+
+route(dublin,berlim,226).
+route(dublin,budapeste,296).
+route(dublin,copenhague,239).
+route(dublin,dubai,1888).
+
+route(havana,lima,1989).
+route(havana,londres,2775).
+route(havana,madri,2594).
+route(havana,mexico,417).
+
+route(hongkong,bombaim,980).
+route(hongkong,dubai,1706).
+route(hongkong,istambul,2080).
+route(hongkong,joanesburgo,2632).
+
+route(istambul,berlim,184).
+route(istambul,bombaim,1729).
+route(istambul,budapeste,271).
+route(istambul,cairo,679).
+
+route(joanesburgo,cairo,1341).
+route(joanesburgo,dubai,1464).
+route(joanesburgo,hongkong,1329).
+route(joanesburgo,londres,1829).
+
+route(lima,buenosaires,1020).
+route(lima,londres,4774).
+route(lima,losangeles,4319).
+route(lima,madri,2030).
+
+route(lisboa,berlim,419).
+route(lisboa,brasilia,3028).
+route(lisboa,budapeste,399).
+route(lisboa,copenhague,405).
+
+route(londres,berlim,326).
+route(londres,bombaim,1600).
+route(londres,budapeste,129).
+route(londres,buenosaires,7434).
+
+route(longyearbyen,oslo,664).
+
+route(losangeles,dubai,3609).
+route(losangeles,dublin,1631).
+route(losangeles,havana,528).
+route(losangeles,hongkong,2381).
+
+route(madri,berlim,365).
+route(madri,budapeste,233).
+route(madri,buenosaires,2809).
+route(madri,cairo,859).
+
+route(marraquexe,lisboa,704).
+route(marraquexe,londres,902).
+route(marraquexe,madri,171).
+route(marraquexe,paris,339).
+
+route(melbourne,dubai,4425).
+route(melbourne,hongkong,1605).
+route(melbourne,losangeles,2608).
+route(melbourne,singapura,351).
+
+route(mexico,buenosaires,4694).
+route(mexico,havana,916).
+route(mexico,lima,789).
+route(mexico,londres,9543).
+
+route(miami,berlim,5218).
+route(miami,brasilia,7073).
+route(miami,buenosaires,5171).
+route(miami,copenhague,7619).
+
+route(montreal,istambul,4291).
+route(montreal,lisboa,2231).
+route(montreal,londres,3656).
+route(montreal,losangeles,689).
+
+route(moscou,berlim,307).
+route(moscou,budapeste,333).
+route(moscou,copenhague,376).
+route(moscou,dubai,508).
+
+route(novaiorque,berlim,1715).
+route(novaiorque,bombaim,3744).
+route(novaiorque,buenosaires,5484).
+route(novaiorque,cairo,2360).
+
+route(oslo,berlim,109).
+route(oslo,budapeste,185).
+route(oslo,copenhague,218).
+route(oslo,dubai,1727).
+
+route(paris,berlim,163).
+route(paris,bombaim,2304).
+route(paris,budapeste,116).
+route(paris,buenosaires,6472).
+
+route(pequim,berlim,1820).
+route(pequim,copenhague,4449).
+route(pequim,dubai,2062).
+route(pequim,hongkong,311).
+
+route(petrolina,recife,149).
+route(petrolina,salvador,153).
+route(petrolina,saopaulo,364).
+
+route(recife,brasilia,375).
+route(recife,lisboa,2310).
+route(recife,petrolina,150).
+route(recife,riodejaneiro,345).
+
+route(reykjavík,berlim,332).
+route(reykjavik,copenhague,348).
+route(reykjavik,dublin,474).
+route(reykjavik,londres,325).
+
+route(riodejaneiro,brasilia,144).
+route(riodejaneiro,buenosaires,1145).
+route(riodejaneiro,dubai,5767).
+route(riodejaneiro,lima,1199).
+
+route(roma,berlim,146).
+route(roma,budapeste,153).
+route(roma,buenosaires,3619).
+route(roma,cairo,796).
+
+route(salvador,brasilia,179).
+route(salvador,buenosaires,1819).
+route(salvador,lisboa,1869).
+route(salvador,petrolina,159).
+
+route(saopaulo,brasilia,205).
+route(saopaulo,buenosaires,981).
+route(saopaulo,dubai,4912).
+route(saopaulo,istambul,2713).
+
+route(singapura,bombaim,976).
+route(singapura,copenhague,4314).
+route(singapura,dubai,1885).
+route(singapura,hongkong,132).
+
+route(toquio,bombaim,10849).
+route(toquio,copenhague,4434).
+route(toquio,dubai,2421).
+route(toquio,hongkong,289).
+
+route(ushuaia,buenosaires,843).
+
+
+
+path(A,B,Path,Len) :- travel(A,B,[A],Q,Len), reverse(Q,Path).
+/* Existe um caminho entre A e B de caminho Path e tamanho Len,
+	se existir uma viagem entre A e B, partindo de [A], percorrendo
+        o caminho Q de tamanho Len: travel(A,B,[A],Q,Len)
+	se a lista reversa do caminho Q se tornar a lista Path: reverse(Q,Path)
+*/
+
+
+travel(A,B,P,[B|P],L) :- route(A,B,L).
+/* Existe uma viagem de A para B, onde P é uma parte dos nós visitados, [B|P] é o caminho percorrido e L é a distância,
+	se exitir uma route de A para B.
+
+*/
+
+
+travel(A,B,Visited,Path,L) :- route(A,C,D), C \== B, \+member(C,Visited), travel(C,B,[C|Visited],Path,L1), L is D+L1.
+/* Existe uma viagem de A para B,
+	se existir uma route de A para C de tamanho D: route(A,C,D)
+	se C é diferente de B: C \== B
+	se C não não foi visitados: \+member(C,Visited)
+	se existir uma viagem de C para B, onde a lista de visitados agora possui C: travel(C,B,[C|Visited],Path,L1)  #RECURSÃO#
+	somando os valores das distâncias em L
+*/
+shortest(A,B,Path,Length) :- setof([P,L],path(A,B,P,L),Set), Set = [_|_], minimal(Set,[Path,Length]).
+
+/*
+ * verdade se os caminhos P, quantidade de nós L e valores V estão
+ * armazenados na coleção Set e L possui o menor valor de toda coleção.
+ */
+shortest(A,B,Path, Len,Value):- setof([P,L, V], (path(A,B,P,V),
+length(P, L)), Set), Set=[_|_], minimal(Set, [Path, Len, Value]).
+
+
+
+minimal([F|R],M) :- min(R,F,M).
+
+% minimal path
+% Se só tem um valor, ele é o menor.
+min([],M,M).
+%se L<M verifica continua verificando min/3 com L sendo a cabeça
+% caso L não seja o menor, no fim esta afirmação é falsa, caso L>M essa
+% afirmação também é falsa (com o cut (!), ele não verifica mais
+% possiveis valores de L
+min([[P,L]|R],[_,M],Min) :- L < M, !, min(R,[P,L],Min).
+min([[P,L,V]|R],[_,M,_],Min) :- L < M, !, min(R,[P,L,V],Min).
+%se L==M, verifica o mais barato.
+min([[P,L,V]|R],[_,M,N],Min) :- L == M,V<N, !, min(R,[P,L,V],Min).
+
+%caso a operação anterior falhe, verifica se M é o menor de todos.
+min([_|R],M,Min) :- min(R,M,Min).
+
+
+
+
+
+
+
+
+
